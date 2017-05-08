a301f2baa4d6605e896ea56e74c98a7bc73d69ec
rlgreen91 <rlgreen91@gmail.com>
2017-05-08 18:18:55 -0500

    Adds service object to do bulk update of squares, also extracts interface module for service objects.

---
+++ app/services/bulk_update_squares.rb
@@ -0,0 +1,10 @@
+class BulkUpdateSquares
+	include Service
+
+	def call(puzzle, values)
+		values.each_pair do |position, value|
+			square = puzzle.get_square_by_position(puzzle.id, position)
+			square.update(value: value)
+		end
+	end
+end
